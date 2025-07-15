9c6d40a3e82df2c0d56344b143202059fd0b0268
JitendraArya <it.developer1@zenithforex.com>
2025-07-15 21:08:43 +0530

    live database url added

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const pool = new Pool({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+//     port: parseInt(process.env.DATABASEPORT),
+// });
 const pool = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: parseInt(process.env.DATABASEPORT),
+    connectionString: process.env.DATABASE_URL,
+    //for live
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
+
 
 export default pool; 
